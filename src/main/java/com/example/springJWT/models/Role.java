aca83c0a4a99dd5ae75931ee6878b6724685ebf4
Wishva Perera <vibushitha99@gmail.com>
2025-06-11 01:21:44 -0400

    Updated Configs and JWT Services

--- src/main/java/com/example/springJWT/models/Role.java
+++ src/main/java/com/example/springJWT/models/Role.java
@@ -1,4 +1,7 @@
 package com.example.springJWT.models;
 
 public enum Role {
+
+    USER,
+    ADMIN
 }
--- src/main/java/com/example/springJWT/repos/UserRepository.java
+++ src/main/java/com/example/springJWT/repos/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.springJWT.repos;
 
-public interface UserRepository {
+import com.example.springJWT.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
 }
